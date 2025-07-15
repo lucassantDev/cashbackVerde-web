ff9dbf4d3d8d30f13758c85f1b1483bc30af145c
Lucas Sant <lucasfdesantana095@gmail.com>
2025-07-15 19:57:59 -0300

    feat: criacao de pasta js

---
+++ js/pagina-inicial.js
@@ -0,0 +1,17 @@
+// function loginGoogle(){
+    
+// }
+
+const loginRetornoPaginaIncial = document.getElementById('fazerLogin-paginaInicial');
+
+loginRetornoPaginaIncial.addEventListener('click', () =>{
+    location = 'pagina-inicial.html';
+    // link da pergunta no stack
+    //https://pt.stackoverflow.com/questions/407028/como-abrir-um-novo-arquivo-html-na-mesma-aba
+});
+
+const cadastroRetornoPaginaInicial = document.getElementById('fazerCadastro-paginaInicial');
+
+cadastroRetornoPaginaInicial.addEventListener('click', () => {
+    location = 'pagina-inicial.html';
+})
